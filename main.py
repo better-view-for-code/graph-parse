2631662e6e92106382fbd5f86320d08aa2824e84
mggger <jiamin.wang@firesoon.com>
2021-04-30 17:53:27 +0800

    init

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import json
+
 from parse.parse import Parser
-from parse.neo import Neop
-from parse.result import ParseResultEntity
-
-
-
 
 if __name__ == '__main__':
     with open("output.json", 'rb') as f:
